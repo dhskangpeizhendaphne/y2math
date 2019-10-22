b5b6663bc3e66434667f9fc532696aa59e335bcd
dhskangpeizhendaphne <38340631+dhskangpeizhendaphne@users.noreply.github.com>
2019-10-22 14:08:04 +0800

    Update t02_simultaneouslinearequations.py

--- t02_simultaneouslinearequations.py
+++ t02_simultaneouslinearequations.py
@@ -1,3 +1,14 @@
 # Find x and y with these two equations:
 # 3x + 4y = 24
 # 4x + 3y = 22
+3x= 24 - 4y 
+4(3x)= 4(24 - 4y) 
+12x = 96 - 16y
+
+4x= 22 - 3y
+3(4x) = 3(22 - 3y)
+12x= 66 - 9y
+
+96 - 16y = 66 - 9y
+-16y + 9y = 66 - 96 
+-7y = 30 
